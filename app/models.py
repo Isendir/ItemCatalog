f8f86aac2b520fdb07ec9a1dfd8f6363f0cbbd24
Ampelio Cherubini <ampelio.cherubini@gmail.com>
2015-01-28 23:36:53 +0100

    sqlalchemy.exc.InvalidRequestError: Class <class 'app.models.Acquisto'> does not have a __table__ or __tablename__ specified and does not inherit from an existing table-mapped class.

---
+++ app/models.py
@@ -0,0 +1,58 @@
+# coding: utf-8
+
+__author__ = 'acherubini'
+
+from app import db
+
+
+class Acquisto(db.Model):
+    __tablename = 'acquisto'
+    id = db.Column(db.Integer, nullable=False),
+    data = db.Column(db.Date, nullable=False),
+    fornitore_id = db.Column(db.ForeignKey('fornitore.id'), nullable=False)
+
+
+class AcquistoDettaglio(db.Model):
+    __tablename__ = 'acquistoDettaglio'
+
+    id = db.Column(db.Integer, primary_key=True)
+    acquisto_id = db.Column(db.ForeignKey('acquisto.id'), nullable=False)
+    qta = db.Column(db.Integer, nullable=False)
+    prezzo = db.Column(db.Integer, nullable=False)
+    iva_id = db.Column(db.ForeignKey('iva.id'), nullable=False)
+
+    acquisto = db.relationship('Acquisto')
+    iva = db.relationship('Iva')
+
+
+class Categoria(db.Model):
+    __tablename__ = 'categoria'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.Text)
+
+
+class Fornitore(db.Model):
+    __tablename__ = 'fornitore'
+
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.Text, nullable=False)
+
+
+class Iva(db.Model):
+    __tablename__ = 'iva'
+
+    id = db.Column(db.Integer, primary_key=True)
+    valore = db.Column(db.Integer, nullable=False)
+    descrizione = db.Column(db.Text, nullable=False)
+
+
+class Prodotto(db.Model):
+    __tablename__ = 'prodotto'
+
+    id = db.Column(db.Integer, primary_key=True)
+    barcode = db.Column(db.Text, nullable=False)
+    descrizione = db.Column(db.Text, nullable=False)
+    categoria = db.Column(db.ForeignKey('categoria.id'))
+
+    categoria1 = db.relationship('Categoria')
